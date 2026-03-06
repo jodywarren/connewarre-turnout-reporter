27b48cf532768eb22e2e0f37294705b29a10a4c8
jodywarren <jodytuuta@msn.com>
2026-03-07 09:34:59 +1100

    Update service-workers.js
    update service worker caching

--- service-workers.js
+++ service-workers.js
@@ -1,18 +1,38 @@
+const CACHE_NAME = "turnout-cache-v2";
+
 self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./styles.css",
+        "./app.js",
+        "./manifest.json",
+        "./members.json"
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
 
-event.waitUntil(
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
 
-caches.open("turnout-cache").then(cache => {
-
-return cache.addAll([
-"/",
-"/index.html",
-"/styles.css",
-"/app.js"
-]);
-
-})
-
-);
-
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
