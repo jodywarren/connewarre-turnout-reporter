f4dcfc7fb12aa59d9d193c08922be22a700476ff
jodywarren <jodytuuta@msn.com>
2026-03-07 00:09:09 +1100

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
+
 function pasteFirs(){
 
 navigator.clipboard.readText()
 .then(text => {
 
 document.getElementById("firs-code").value = text;
+
+})
+.catch(err => {
+
+alert("Clipboard paste failed");
+
+});
+
+}
+
+
+/* Register service worker for offline support */
+
+if("serviceWorker" in navigator){
+
+window.addEventListener("load", () => {
+
+navigator.serviceWorker.register("service-worker.js")
+.then(() => {
+
+console.log("Service worker registered");
+
+});
 
 });
 
