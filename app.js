09d6415249860bbe217e2d15c970c8989cf81441
jodywarren <jodytuuta@msn.com>
2026-03-07 10:04:37 +1100

    Update app.js

--- app.js
+++ app.js
@@ -49,14 +49,13 @@ console.log("Service worker failed:", err);
 }
 document.addEventListener("DOMContentLoaded", function(){
 
-const now = new Date();
+  const now = new Date();
+  const date = now.toISOString().split("T")[0];
 
-const date = now.toISOString().split("T")[0];
+  document.getElementById("pager-date").value = date;
+  document.getElementById("pager-time").value = now.toTimeString().slice(0,5);
 
-document.getElementById("pager-date").value = date;
-
-document.getElementById("pager-time").value =
-now.toTimeString().slice(0,5);
+  loadMembers();
 
 });
 let membersData = {
